24aa4c24479fc32a369e307d01b1a792f83728d5
oumaimabelabbas <oumaimabelabbas2003@gmail.com>
2025-02-16 20:51:50 +0100

    1st_commit

---
+++ index.js
@@ -0,0 +1,7 @@
+let sent1;
+document.getElementById("Yes").onclick = function(){
+  document.getElementById("love").textContent="Hta ana kanmot 3lik <3"
+}
+document.getElementById("No").onclick = function(){
+  document.getElementById("love").textContent="What do you mean by NOOOOO"
+}
